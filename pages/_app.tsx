31a90bef684d7d8ba0f0801ecbb0fdb63f6dd380
martijn van de ree <mrtjnvanderee@gmail.com>
2022-06-10 14:54:55 +0200

    removed excess code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,14 +13,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <ChakraProvider theme={theme}>
-      <Script
-        src="https://widget.Cloudinary.com/v2.0/global/all.js"
-        type="text/javascript"
-        key="cloudinary"
-      />
-      <Script id="my-script" src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-        key="netlify" />
-
       <Component {...pageProps} />
     </ChakraProvider>
   );
